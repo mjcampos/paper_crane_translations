d7954e2a3b8b265fffc81c9cd0697f9f614206d9
Michael Campos <camposmj89@gmail.com>
2015-11-22 15:09:59 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(express.static(path.join(__dirname, "./app")));
+app.use(bodyParser.json());
+
+app.listen(8000, function() {
+	console.log("Program active");
+});
