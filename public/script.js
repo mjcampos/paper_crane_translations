91883707fc3a423237d264e12ce9d54e37f460f3
Michael Campos <camposmj89@gmail.com>
2015-12-02 22:42:37 -0800

    Finally got the content details, both in German and English. I only have enough time today to add the english content. Nick has asked to change the page names and to include an additional page for each language. He has also asked to change the background image to a map of Europe with special emphasis on Germany and the UK and that I create the site logo.

--- public/script.js
+++ public/script.js
@@ -3,9 +3,10 @@ var myApp = angular.module('myApp', ['ngRoute']);
 // Clientside routing
 myApp.config(function($routeProvider) {
 	$routeProvider
-	.when('/', {templateUrl: 'partials/english/home.html'})
-	.when('/english/payment', {templateUrl: 'partials/english/payment.html'})
-	.when('/english/legal', {templateUrl: 'partials/english/legal.html'})
+	.when('/', {templateUrl: 'partials/english/profile.html'})
+	.when('/english/services', {templateUrl: 'partials/english/services.html'})
+	.when('/english/prices', {templateUrl: 'partials/english/prices.html'})
+	.when('/english/contact_information', {templateUrl: 'partials/english/contact_information.html'})
 	.when('/german/home', {templateUrl: 'partials/german/home.html'})
 	.when('/german/payment', {templateUrl: 'partials/german/payment.html'})
 	.when('/german/legal', {templateUrl: 'partials/german/legal.html'})
