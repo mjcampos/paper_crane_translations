469d69709a90e6da927a5835342f0f96601834b4
Michael Campos <camposmj89@gmail.com>
2015-12-05 20:58:05 -0800

    Added German content. Now need to work on the logo and background images.

--- public/script.js
+++ public/script.js
@@ -7,8 +7,9 @@ myApp.config(function($routeProvider) {
 	.when('/english/services', {templateUrl: 'partials/english/services.html'})
 	.when('/english/prices', {templateUrl: 'partials/english/prices.html'})
 	.when('/english/contact_information', {templateUrl: 'partials/english/contact_information.html'})
-	.when('/german/home', {templateUrl: 'partials/german/home.html'})
-	.when('/german/payment', {templateUrl: 'partials/german/payment.html'})
-	.when('/german/legal', {templateUrl: 'partials/german/legal.html'})
+	.when('/german/profil', {templateUrl: 'partials/german/profil.html'})
+	.when('/german/service', {templateUrl: 'partials/german/service.html'})
+	.when('/german/preise', {templateUrl: 'partials/german/preise.html'})
+	.when('/german/kontakt', {templateUrl: 'partials/german/kontakt.html'})
 	.otherwise({redirectTo: '/'});
 });
