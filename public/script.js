b76ca97ddab67f5529eb029a08605469cb254412
Michael Campos <camposmj89@gmail.com>
2015-12-06 23:27:33 -0800

    Simplified the contact_information.html page to contact.html and changed the routing to match that.

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ myApp.config(function($routeProvider) {
 	.when('/', {templateUrl: 'partials/english/profile.html'})
 	.when('/english/services', {templateUrl: 'partials/english/services.html'})
 	.when('/english/prices', {templateUrl: 'partials/english/prices.html'})
-	.when('/english/contact_information', {templateUrl: 'partials/english/contact_information.html'})
+	.when('/english/contact', {templateUrl: 'partials/english/contact.html'})
 	.when('/german/profil', {templateUrl: 'partials/german/profil.html'})
 	.when('/german/service', {templateUrl: 'partials/german/service.html'})
 	.when('/german/preise', {templateUrl: 'partials/german/preise.html'})
