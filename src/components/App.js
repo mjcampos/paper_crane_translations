29783af6fce0feed4135a086fc1e859437492e77
Michael Campos <camposmj89@gmail.com>
2019-02-28 11:47:14 -0800

    Refractored App and Nav components into stateless components

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Nav from './Nav';
 import English from '../english/English';
 import German from '../german/German';
 import {Route} from 'react-router-dom';
 
-class App extends Component {
-	render() {
-		return (
-			<div>
-				<Nav/>
+const App = () => {
+	return (
+		<div>
+			<Nav/>
 
-				<Route exact path="/" component={English}/>
-				<Route exact path="/german" component={German}/>
-			</div>
-		);
-	}
+			<Route exact path="/" component={English}/>
+			<Route exact path="/german" component={German}/>
+		</div>
+	);
 }
 
 export default App;
