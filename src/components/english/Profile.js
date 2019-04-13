0f686a3555baf9890a2c790f36056d8477af1256
Michael Campos <camposmj89@gmail.com>
2019-04-12 19:07:38 -0700

    Connected the two language profiles to display facts at the bottom of their home tabs.

--- src/components/english/Profile.js
+++ src/components/english/Profile.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import {Row, Col} from 'react-bootstrap';
 import NickImage from '../shared/NickImage';
 import InputBox from '../shared/InputBox';
 
-const Profile = () => {
-	return (
-		<div>
-			<h1 className="text-center">Profile</h1>
+class Profile extends Component {
+	render() {
+		return (
+			<div>
+				<h1 className="text-center">Profile</h1>
 
-			<Row>
-				<Col sm={9}>
-					<p>Translating is the proper rendering of a statement into another language. An adept translator requires professional and practical skill, years of experience and a native speaker’s intuition of the target languages. The lack of competency of the consumer in the language that a text is to be translated in means that the quality of a translation often eludes a client. Trust is the best quality assurance for customers of translation services.</p>
-					<p>I am a native bilingual German-English speaker with a bachelor’s degree in German and I’m currently working on a Master’s Degree in Linguistics. I am educated in the linguistic structures of both English and German and, simultaneously, I have a native intuition of both languages. Furthermore, I have years of translating experience. If you need something translated into either English or German or need Language Tutoring, then I’m your ideal candidate for getting the job done!</p>
-				</Col>
-				
-				<Col sm={3}>
-					<NickImage/>
-					<InputBox/>
-				</Col>
-			</Row>
-		</div>
-	);
+				<Row>
+					<Col sm={9}>
+						<p>Translating is the proper rendering of a statement into another language. An adept translator requires professional and practical skill, years of experience and a native speaker’s intuition of the target languages. The lack of competency of the consumer in the language that a text is to be translated in means that the quality of a translation often eludes a client. Trust is the best quality assurance for customers of translation services.</p>
+						<p>I am a native bilingual German-English speaker with a bachelor’s degree in German and I’m currently working on a Master’s Degree in Linguistics. I am educated in the linguistic structures of both English and German and, simultaneously, I have a native intuition of both languages. Furthermore, I have years of translating experience. If you need something translated into either English or German or need Language Tutoring, then I’m your ideal candidate for getting the job done!</p>
+					</Col>
+					
+					<Col sm={3}>
+						<NickImage/>
+						<InputBox/>
+					</Col>
+				</Row>
+
+				<Row>
+					<Col sm={12}>
+						<p>{this.props.facts}</p>
+					</Col>
+				</Row>
+			</div>
+		);
+	}
 }
 
-export default Profile;
+var mapStateToProps = state => state;
+
+export default connect(mapStateToProps, null)(Profile);
--- src/components/german/Profil.js
+++ src/components/german/Profil.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import {Row, Col} from 'react-bootstrap';
 import NickImage from '../shared/NickImage';
 import InputBox from '../shared/InputBox';
 
-const Profil = () => {
-	return (
-		<div>
-			<h1 className="text-center">Profil</h1>
+class Profil extends Component {
+	render() {
+		return (
+			<div>
+				<h1 className="text-center">Profil</h1>
 
-			<Row>
-				<Col sm={9}>
-					<p>&Uuml;bersetzung ist die pr&auml;zise Wiedergabe eines Ausdrucks in einer anderen Sprache. Ein erfahrener &Uuml;bersetzer ben&ouml;tigt professionelle und praktische Kompetenz, jahrelange Erfahrung und die muttersprachliche Intuition der Zielsprachen. Die geringere Kompetenz des Auftraggebers in den Zielsprachen f&uuml;hrt h&auml;ufig dazu, dass dieser die Qualit&auml;t einer &Uuml;bersetzung kaum angemessen einsch&auml;tzen kann. Vertrauen ist daher die beste Qualit&auml;tssicherung f&uuml;r Kunden von &Uuml;bersetzungsdienstleistungen.</p>
-					<p>Ich bin ein bilingual Englisch-Deutscher Muttersprachler mit einem Bachelor in Germanistik und arbeite aktuell an meinem Master in Linguistik (Sprachwissenschaften). Ich verf&uuml;ge &uuml;ber die formale Bildung in den linguistischen Strukturen des Deutschen und Englischen und besitze gleichzeitig die muttersprachliche Intuition beider Sprachen. Hinzu kommt meine mehrj&auml;hrige Erfahrung mit &Uuml;bersetzungen. Wenn Sie etwas &uuml;bersetzt haben möchten oder Nachhilfe in deutscher oder englischer Sprache ben&ouml;tigen, dann bin ich genau der Richtige f&uuml;r den Job!</p>
-				</Col>
-				
-				<Col sm={3}>
-					<NickImage/>
-					<InputBox/>
-				</Col>
-			</Row>
-		</div>
-	);
+				<Row>
+					<Col sm={9}>
+						<p>&Uuml;bersetzung ist die pr&auml;zise Wiedergabe eines Ausdrucks in einer anderen Sprache. Ein erfahrener &Uuml;bersetzer ben&ouml;tigt professionelle und praktische Kompetenz, jahrelange Erfahrung und die muttersprachliche Intuition der Zielsprachen. Die geringere Kompetenz des Auftraggebers in den Zielsprachen f&uuml;hrt h&auml;ufig dazu, dass dieser die Qualit&auml;t einer &Uuml;bersetzung kaum angemessen einsch&auml;tzen kann. Vertrauen ist daher die beste Qualit&auml;tssicherung f&uuml;r Kunden von &Uuml;bersetzungsdienstleistungen.</p>
+						<p>Ich bin ein bilingual Englisch-Deutscher Muttersprachler mit einem Bachelor in Germanistik und arbeite aktuell an meinem Master in Linguistik (Sprachwissenschaften). Ich verf&uuml;ge &uuml;ber die formale Bildung in den linguistischen Strukturen des Deutschen und Englischen und besitze gleichzeitig die muttersprachliche Intuition beider Sprachen. Hinzu kommt meine mehrj&auml;hrige Erfahrung mit &Uuml;bersetzungen. Wenn Sie etwas &uuml;bersetzt haben möchten oder Nachhilfe in deutscher oder englischer Sprache ben&ouml;tigen, dann bin ich genau der Richtige f&uuml;r den Job!</p>
+					</Col>
+					
+					<Col sm={3}>
+						<NickImage/>
+						<InputBox/>
+					</Col>
+				</Row>
+
+				<Row>
+					<Col sm={12}>
+						<p>{this.props.facts}</p>
+					</Col>
+				</Row>
+			</div>
+		);
+	}
 }
 
-export default Profil;
+var mapStateToProps = state => state;
+
+export default connect(mapStateToProps, null)(Profil);
