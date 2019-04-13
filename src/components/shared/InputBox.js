cacea9f7df62372545ee9a4c0463773f2ba6cf59
Michael Campos <camposmj89@gmail.com>
2019-04-12 18:25:56 -0700

    Build out the form for the InputBox component. Now need data.

--- src/components/shared/InputBox.js
+++ src/components/shared/InputBox.js
@@ -3,12 +3,25 @@ import {connect} from 'react-redux';
 import {Form} from 'react-bootstrap';
 
 class Inputbox extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			code: ""
+		}
+	}
+
+	handleSubmit = e => {
+		e.preventDefault();
+		console.log(this.state);
+	}
+
 	render() {
+		var requiredClicks = 0;
+
 		return (
-			<Form hidden={this.props.clicks >= 3 ? false : true}>
-				<Form.Group>
-					<Form.Control type="text" placeholder="Something"/>
-				</Form.Group>
+			<Form hidden={this.props.clicks >= requiredClicks ? false : true} onSubmit={this.handleSubmit}>
+				<Form.Control value={this.state.code} onChange={(e) => this.setState({code: e.target.value})}/>
 			</Form>
 		);
 	}
